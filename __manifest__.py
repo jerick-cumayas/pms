0df38b10bf44e330f12753cf51dd5057f64d8a65
Jerick Cumayas <cumayas.jerick@gmail.com>
2026-01-05 10:37:43 +0800

    [Initial] Initial commit for pms module

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'PMS',
+    'version': '1.0',
+    'category': 'Services',
+    'depends': ['base', 'contacts', 'account', 'website', 'website_blog'],
+    'data': [ 
+    ],
+    'license': 'LGPL-3',
+    'author': 'Jerick Cumayas'
+}
