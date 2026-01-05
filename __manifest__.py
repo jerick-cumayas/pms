4a9afec32976ac1a79fa088efeb5891d3ca06ddc
jerick-cumayas <cumayas.jerick@gmail.com>
2026-01-05 17:59:47 +0800

    [Feature] Added data files
    - added policies(pest, noise, cctv, pet)
    - added amenities guidelines(basketball, pool)

--- __manifest__.py
+++ __manifest__.py
@@ -2,8 +2,20 @@
     'name': 'PMS',
     'version': '1.0',
     'category': 'Services',
-    'depends': ['base', 'contacts', 'account', 'website', 'website_blog'],
+    'depends': [
+        'base', 
+        'contacts', 
+        'account', 
+        'helpdesk',
+        'website', 
+        'website_blog',
+    ],
     'data': [ 
+        'data/announcements/sample_posts.xml',
+        'data/announcements/amenities.xml',
+        'data/announcements/policies.xml',
+        
+        'data/teams.xml',
     ],
     'license': 'LGPL-3',
     'author': 'Jerick Cumayas'
